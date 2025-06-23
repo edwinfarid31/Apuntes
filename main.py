294b242b5f5d3bf1016e5e37a9a6c47d95a18b1f
Farid <edwinfarid316@gmail.com>
2025-06-22 21:04:18 -0500

    uno

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello World")
-=======
-def saludo(nombre):
-    print(f"Hola {nombre}")
-
-saludo("Farid")
->>>>>>> parent of 4763dfa (cambio saludo)
-=======
-saludo = "Hello world"
-print(saludo)
->>>>>>> parent of d58c58d (Creacion de la funcion saludo)
+#hola uno 
